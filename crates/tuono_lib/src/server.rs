ba79b710d66d41ce5b0cbaca5aa5d4833a274037
Per Magnus Veierland <pveierland@gmail.com>
2025-03-24 12:01:05 +0800

    feat: add server compression

--- crates/tuono_lib/src/server.rs
+++ crates/tuono_lib/src/server.rs
@@ -4,6 +4,7 @@ use crate::mode::{Mode, GLOBAL_MODE};
 use axum::routing::{get, Router};
 use colored::Colorize;
 use ssr_rs::Ssr;
+use tower_http::compression::CompressionLayer;
 use tower_http::services::ServeDir;
 use tuono_internal::config::Config;
 
@@ -95,10 +96,17 @@ impl Server {
                 .await
                 .expect("Failed to serve development server");
         } else {
+            let compression_layer: CompressionLayer = CompressionLayer::new()
+                .br(true)
+                .deflate(true)
+                .gzip(true)
+                .zstd(true);
+
             let router = self
                 .router
                 .to_owned()
                 .layer(LoggerLayer::new())
+                .layer(compression_layer)
                 .fallback_service(
                     ServeDir::new(PROD_PUBLIC_DIR)
                         .fallback(get(catch_all).layer(LoggerLayer::new())),
